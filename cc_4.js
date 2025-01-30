b0a29483fcdca885bf2e5bfb4ae4648e2e3f8b6e
Riley Hedges <rhedges1@usf.edu>
2025-01-30 18:30:28 -0500

    Task 3 - Inventory Depletion

--- cc_4.js
+++ cc_4.js
@@ -8,7 +8,7 @@ console.log('Task 1: If Statements');
 let purchaseAmount = 150;
 if (purchaseAmount > 100) {
     purchaseAmount *= .9; // Applying 10% discount
-};
+}
 console.log(`Final amount after discount: $${purchaseAmount}`); 
 
 //////////////////////
@@ -25,3 +25,16 @@ for (let index = 0; index < sales.length; index++) {
     sum += sales[index];
 }
 console.log(`Sales Total: $${sum}`)
+
+/////////////////////////
+// Task 3: While Loops //
+/////////////////////////
+
+console.log('--------------------------------------');
+console.log('Task 3: While Loops');
+
+let stock = 10;
+while (stock >= 0) {
+    console.log("Stock Value Decreasing:", stock);
+    stock--;
+}
