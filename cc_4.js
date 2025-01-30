2679661242f6865f104ae843596aac4d3c5c10c8
Riley Hedges <rhedges1@usf.edu>
2025-01-30 18:22:57 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,13 @@
+///////////////////////////
+// Task 1: If Statements //
+///////////////////////////
+
+console.log('--------------------------------------');
+console.log('Task 1: If Statements');
+
+let purchaseAmount = 150;
+if (purchaseAmount > 100) {
+    purchaseAmount *= .9; // Applying 10% discount
+};
+console.log(`Final amount after discount: $${purchaseAmount}`); 
+
