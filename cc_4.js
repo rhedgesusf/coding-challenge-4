11298a212804fb5ae0968ad611ee1eaf3cb53ba0
Riley Hedges <rhedges1@usf.edu>
2025-01-30 19:36:10 -0500

    Task 8 - Tax Calculation

--- cc_4.js
+++ cc_4.js
@@ -95,3 +95,22 @@ orders.forEach((order, index) => {
     console.log(`Order ${index + 1}: ${order}`);
 });
 
+//////////////////////////////////
+// Task 8: Function Declaration //
+//////////////////////////////////
+
+console.log('--------------------------------------');
+console.log('Task 8: Function Declaration');
+
+function calculateTax(amount, taxRate) {
+    return amount * taxRate;
+}
+
+let num = 100;
+let taxRate = .07;
+
+
+console.log(`Tax Amount: $${calculateTax(num, taxRate).toFixed(2)}`);
+
+
+
