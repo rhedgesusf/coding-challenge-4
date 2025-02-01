6c3b2525c19b60ef266cbfef4fed4ec8be667e21
Riley Hedges <rhedges1@usf.edu>
2025-01-31 19:09:03 -0500

    Added Comments

--- cc_4.js
+++ cc_4.js
@@ -2,113 +2,123 @@
 // Task 1: If Statements //
 ///////////////////////////
 
-console.log('--------------------------------------');
-console.log('Task 1: If Statements');
+console.log("--------------------------------------");
+console.log("Task 1: If Statements");
 
 let purchaseAmount = 150;
 if (purchaseAmount > 100) {
-    purchaseAmount *= .9; // Applying 10% discount
+  purchaseAmount *= 0.9; // Applying 10% discount
 }
-console.log(`Final amount after discount: $${purchaseAmount}`); 
+
+console.log(`Final amount after discount: $${purchaseAmount}`);
 
 //////////////////////
 // Task 2: For Loop //
 //////////////////////
 
-console.log('--------------------------------------');
-console.log('Task 2: For Loop');
+console.log("--------------------------------------");
+console.log("Task 2: For Loop");
 
 let sales = [98, 76, 110, 36, 236];
 let sum = 0;
 
+// Calculate total sales
 for (let index = 0; index < sales.length; index++) {
-    sum += sales[index];
+  sum += sales[index];
 }
-console.log(`Sales Total: $${sum}`)
+
+console.log(`Sales Total: $${sum}`);
 
 /////////////////////////
 // Task 3: While Loops //
 /////////////////////////
 
-console.log('--------------------------------------');
-console.log('Task 3: While Loops');
+console.log("--------------------------------------");
+console.log("Task 3: While Loops");
 
+// Decrease stock until 0
 let stock = 10;
 while (stock >= 0) {
-    console.log(`Stock Value Decreasing: ${stock}`);
-    stock--;
+  console.log(`Stock Value Decreasing: ${stock}`);
+  stock--;
 }
 
 /////////////////////////////
 // Task 4: Do...While Loop //
 /////////////////////////////
 
-console.log('--------------------------------------');
-console.log('Task 4: Do...While Loop');
+console.log("--------------------------------------");
+console.log("Task 4: Do...While Loop");
 
 let responses = 0;
 
+// Collects responses until it reaches 3
 do {
-    console.log(`Responses: ${responses}`);
-    responses++
+  console.log(`Responses: ${responses}`);
+  responses++;
 } while (responses <= 3);
 
 ////////////////////////////
 // Task 5: For... In Loop //
 ////////////////////////////
 
-console.log('--------------------------------------');
-console.log('Task 5: For... In Loop');
+console.log("--------------------------------------");
+console.log("Task 5: For... In Loop");
 
 let employee = {
-    name: "Bill",
-    position: "Manager",
-    salary: 80000
+  name: "Bill",
+  position: "Manager",
+  salary: 80000,
 };
 
+// Log object properties
 for (let property in employee) {
-    console.log(`${property}: ${employee[property]}`);
+  console.log(`${property}: ${employee[property]}`);
 }
 
 ///////////////////////////
 // Task 6: For...Of Loop //
 ///////////////////////////
 
-console.log('--------------------------------------');
-console.log('Task 6: For...Of Loop');
+console.log("--------------------------------------");
+console.log("Task 6: For...Of Loop");
+
 
 let products = ["Laptop", "Keyboard", "Monitor"];
+
+// Display each product name
 for (let product of products) {
-    console.log(`Product Name: ${product}`);
+  console.log(`Product Name: ${product}`);
 }
 
 //////////////////////////////
 // Task 7: forEach() Method //
 //////////////////////////////
 
-console.log('--------------------------------------');
-console.log('Task 7: forEach() Method');
+console.log("--------------------------------------");
+console.log("Task 7: forEach() Method");
 
-let orders = [10,11,12,13,14];
+let orders = [10, 11, 12, 13, 14];
 
+// Log order IDs
 orders.forEach((order, index) => {
-    console.log(`Order ${index + 1}: ${order}`);
+  console.log(`Order ${index + 1}: ${order}`);
 });
 
 //////////////////////////////////
 // Task 8: Function Declaration //
 //////////////////////////////////
 
-console.log('--------------------------------------');
-console.log('Task 8: Function Declaration');
+console.log("--------------------------------------");
+console.log("Task 8: Function Declaration");
 
+// Calculates Tax
 function calculateTax(amount, taxRate) {
-    return amount * taxRate;
+  return amount * taxRate;
 }
 
 let num = 100;
-let taxRate = .07;
-
+let taxRate = 0.07;
 
 console.log(`Tax Amount: $${calculateTax(num, taxRate).toFixed(2)}`);
 
@@ -116,13 +126,15 @@ console.log(`Tax Amount: $${calculateTax(num, taxRate).toFixed(2)}`);
 // Task 9: Function Expressions //
 //////////////////////////////////
 
-console.log('--------------------------------------');
-console.log('Task 9: Function Expressions');
+console.log("--------------------------------------");
+console.log("Task 9: Function Expressions");
 
-let applyDiscount = function(price, discount) {
-    return price * (1 - discount);
-}
-let discount = .05;
+// Calculates Discount
+let applyDiscount = function (price, discount) {
+  return price * (1 - discount);
+};
+
+let discount = 0.05;
 let price = 500;
 
 console.log(`Discounted Price: $${applyDiscount(price, discount)}`);
@@ -131,13 +143,12 @@ console.log(`Discounted Price: $${applyDiscount(price, discount)}`);
 // Task 10: Arrow Functions //
 //////////////////////////////
 
-console.log('--------------------------------------');
-console.log('Task 10: Arrow Functions');
+console.log("--------------------------------------");
+console.log("Task 10: Arrow Functions");
 
-// Declaring function that calculates points based off of purchase
-const calculatePoints = (totalPurchase) => totalPurchase * .1;
+// Calculates points based off of purchase
+const calculatePoints = (totalPurchase) => totalPurchase * 0.1;
 
 let purchase = 125;
 let points = calculatePoints(purchase);
 console.log(`Points Accumulated: ${Math.floor(points)}`); // math.floor rounds down
-
